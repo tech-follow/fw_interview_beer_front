efd138050f1212060743cec6906e3d807d5a44fc
thibaut.cointet <thibaut.cointet@ekino.com>
2023-12-04 20:30:50 +0100

    feat(rate): display user's rating and RenderCount result

--- front/src/components/BeerItem/BeerItem.tsx
+++ front/src/components/BeerItem/BeerItem.tsx
@@ -1,16 +1,53 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 import styles from "./BeerItem.module.scss";
 import { Rate } from "./Rate/Rate";
 import { BeerItemProps } from "./BeerItem.model";
+import { RenderCount } from "../RenderCount/RenderCount";
 
-export const BeerItem: FC<BeerItemProps> = ({ beer: { name, ibu, score } }) => (
-  <div className={styles.beerItem}>
-    <div className={styles.name}>{name}</div>
-    <div className={styles.ibu}>IBU: {ibu}</div>
-    <div className={styles.score}>Score: {score.toPrecision(1)}</div>
-    <div className={styles.rate}>
-      <Rate />
-    </div>
-  </div>
-);
+export const BeerItem: FC<BeerItemProps> = ({
+  beer: { name, ibu, score, uuid },
+}) => {
+  const [selectedScore, setSelectedScore] = useState(null);
+
+  const handleRate = (newScore: string) => {
+    fetch(`http://localhost:3000/api/beers/${uuid}/rate`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ score: newScore }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setSelectedScore(newScore);
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la notation de la bière:", error);
+      });
+  };
+
+  return (
+    <>
+      <RenderCount currentComponent="BeerItem" childrenName="Rate" />
+      <div className={styles.beerItem}>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.ibu}>IBU: {ibu}</div>
+        <div className={styles.score}>Score: {score.toPrecision(1)}</div>
+        {selectedScore !== null && (
+          <div className={styles.score}>
+            <span> Your score : {selectedScore}</span>
+          </div>
+        )}
+        <div className={styles.rate}>
+          <Rate onRate={handleRate} />
+        </div>
+      </div>
+    </>
+  );
+};
--- front/src/components/BeerItem/Rate/Rate.jsx
+++ front/src/components/BeerItem/Rate/Rate.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 import styles from "./Rate.module.scss";
+
 export const Rate = ({ onRate }) => (
-  <div className={styles.rate}>
-    <div className={styles.inLove} />
-    <div className={styles.happy} />
-    <div className={styles.sad} />
-  </div>
+  <>
+    <div className={styles.rate}>
+      <div className={styles.inLove} onClick={() => onRate(5)} />
+      <div className={styles.happy} onClick={() => onRate(3)} />
+      <div className={styles.sad} onClick={() => onRate(1)} />
+    </div>
+  </>
 );
